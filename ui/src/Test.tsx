c0a02efe45f03118eadd5dafb0c0a47661065f55
Felix Wieland <felix.wieland@mail.de>
2022-10-11 22:43:46 +0200

    some ui changes

--- ui/src/Test.tsx
+++ ui/src/Test.tsx
@@ -28,8 +28,10 @@ function Test() {
                         unsubscribe
                     }) => <Show when={subscribed()}>
                             <div class="p-2 border mb-2 rounded-lg bg-zinc-50 dark:bg-zinc-700 dark:border-zinc-600 shadow-sm relative">
-                                <button class="absolute right-1 top-1 rounded bg-teal-700 w-6 h-6 text-white hover:bg-teal-900 transition-colors" onClick={unsubscribe}>
-                                    X
+                                <button class="absolute right-1.5 top-1.5 rounded bg-teal-700 w-6 h-6 text-white hover:bg-teal-900 transition-colors" onClick={unsubscribe}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
                                 </button>
                                 <label for="endpoint" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-200">Node: {node().id}</label>
                                 <ControlledInput
